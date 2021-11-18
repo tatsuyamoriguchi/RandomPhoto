9deb6bd71284187a6895e62424216167128b7698
Brian Moriguchi <brian@beckos.com>
2021-11-18 14:29:17 -0800

    Initial Commit

---
+++ RandomPhoto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RandomPhoto
+//
+//  Created by Tatsuya Moriguchi on 11/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RandomPhoto/RandomPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomPhotoApp.swift
+//  RandomPhoto
+//
+//  Created by Tatsuya Moriguchi on 11/18/21.
+//
+
+import SwiftUI
+
+@main
+struct RandomPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
